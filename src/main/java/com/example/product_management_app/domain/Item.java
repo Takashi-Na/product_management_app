f7b345c7756978f4daa3f7e69cb019c1330e03df
Takashi-Na <69133170+Takashi-Na@users.noreply.github.com>
2021-05-31 17:39:44 +0900

    bootstrap導入

---
+++ src/main/java/com/example/product_management_app/domain/Item.java
@@ -0,0 +1,20 @@
+package com.example.product_management_app.domain;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Item {
+  private Long id;
+  
+  @NotBlank(message = "商品名を入力してください")
+  private String name;
+
+  @Min(value = 10, message = "10以上の数値を入力してください")
+  @Max(value = 10000, message = "10000以下の数字を入力してください")
+  private float price;
+
+  @Size(max = 50, message = "ベーダー名は50文字を超えないでください")
+  private String vendor;
+}
