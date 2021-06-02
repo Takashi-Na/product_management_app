1b55c531c1a9ad30594e4f667b687ecbca82ab1e
Takashi-Na <69133170+Takashi-Na@users.noreply.github.com>
2021-06-02 15:20:58 +0900

    add controller

---
+++ src/main/java/com/example/product_management_app/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.product_management_app.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class HomeController {
+  @RequestMapping("/")
+  public String index() {
+    return "/index.html";
+  }
+
+}
